77c03f791b3f6d4005d3f90d3808e20f528c0feb
ashan tharaka <ashantharaka715@gmail.com>
2021-08-30 17:12:02 +0530

    add some operations

---
+++ Game2/Main.java
@@ -0,0 +1,32 @@
+package Games.Game2;
+
+import java.util.Random;
+
+class SnakeGame{
+
+}
+class Snake{
+   static int Len=5;
+   static int headx;
+   static int heady;
+   static int tailx;
+   static int taily;
+
+
+}
+class Food{
+   static int x=0;
+   static int y=0;
+
+   Random random=new Random();
+
+
+
+
+
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ashan");
+    }
+}
